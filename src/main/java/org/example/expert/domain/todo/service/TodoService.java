26aa4f23e05c8e291b84c734b5d50557b85cb0d8
jinhee kim <lsa68803@naver.com>
2025-09-16 20:28:27 +0900

    feat: [Level.1-1] @Transactional의 이해 완료

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -19,12 +19,15 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
+// 1-1. " readonly = true " 가 전체 메서드에 적용되면 안됨 (무분별 사용 금지)
+// -> 읽기 전용에만 사용!
 public class TodoService {
 
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
 
+    // todo 생성
     public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
         User user = User.fromAuthUser(authUser);
 
@@ -47,6 +50,8 @@ public class TodoService {
         );
     }
 
+    // todo 조회
+    @Transactional(readOnly = true)
     public Page<TodoResponse> getTodos(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
@@ -63,6 +68,8 @@ public class TodoService {
         ));
     }
 
+    // 특정 todo 조회
+    @Transactional(readOnly = true)
     public TodoResponse getTodo(long todoId) {
         Todo todo = todoRepository.findByIdWithUser(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
