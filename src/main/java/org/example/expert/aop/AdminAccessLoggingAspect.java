14532df1388c49c2e21c7c64d5b5020c96c84970
jinhee kim <lsa68803@naver.com>
2025-09-17 20:13:42 +0900

    feat: [Level.1-5] AOP의 이해 완료 (After -> Before 로 수정)

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -4,22 +4,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
 @Slf4j
-@Aspect
+@Aspect    // AOP 선언! -> 여러 Advice / Pointcut 담는 모듈
 @Component
 @RequiredArgsConstructor
 public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
-    @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
-    public void logAfterChangeUserRole(JoinPoint joinPoint) {
+    // [1-5] AOP 수정 완료
+    // - @After -> @Before : changeUserRole() 메서드 실행 전 동작 완료
+    // - 메서드명 수정 (AOP 의도에 맞도록 Before로 수정)
+    @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
+    // @Before : 어드바이스 -> AOP 의 핵심 역할!
+    // execution : Pointcut -> 어떤 메서드 실행 시점에 적용할지 지정하는 부분
+    public void logBeforeChangeUserRole(JoinPoint joinPoint) {
+    // -> 실제 실행될 부가 기능(로깅 로직)
+    // JoinPoint : 현재 실행 중인 메서드의 시그니처/인자 정보 접근 가능
         String userId = String.valueOf(request.getAttribute("userId"));
         String requestUrl = request.getRequestURI();
         LocalDateTime requestTime = LocalDateTime.now();
