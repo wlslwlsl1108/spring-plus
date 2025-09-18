a7e5999bfe72c855409b8a586b2e588b287eefe9
jinhee kim <lsa68803@naver.com>
2025-09-18 15:36:27 +0900

    feat: [Level.2-7] N+1 문제 완료(Comment-JPQL 쿼리 수정)

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,10 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // [2-7] N+1 문제 해결 완료!
+    // DISTINCT : LEFT JOIN 시, 한 유저가 여러개의 댓글과 조인되면서 댓글이 중복으로 나올 수 있기 때문에 사용
+    // FETCH JOIN : 연관관계 맺어진 테이블의 데이터를 한 번의 쿼리로 가져오기 때문에 사용
+    // @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId") => N+1 문제 발생
+    @Query("SELECT DISTINCT c FROM Comment c LEFT JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
