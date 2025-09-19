76b6c84d6f858d99b575f65964db02154e0ae291
jinhee kim <lsa68803@naver.com>
2025-09-19 17:35:32 +0900

    feat: [Level.2-8] QueryDSL 전용 환경 설정 클래스(QueryDSLConfig) 생성

--- src/main/java/org/example/expert/config/QueryDSLConfig.java
+++ src/main/java/org/example/expert/config/QueryDSLConfig.java
@@ -6,8 +6,14 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// [2-8] QueryDSL 적용 완료
+// QueryDSL 전용 환경 설정 클래스
+// 프로젝트 전역에서 쓸 수 있는 JPAQueryFactory 빈 등록
+// 이게 없으면, 서비스/레포 에서 JPAQueryFactory 를 바로 주입 받을 수 없고,
+// new 해서 직접 만들어야 함
+// 유지보수성/확장성 위해 Config 로 Bean 등록해주는 게 표준!
 @Configuration
-public class JPAConfiguration {
+public class QueryDSLConfig {
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -17,3 +23,4 @@ public class JPAConfiguration {
         return new JPAQueryFactory(entityManager);
     }
 }
+
